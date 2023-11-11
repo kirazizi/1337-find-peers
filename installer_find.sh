4b3c3b1d8f151663a39ca0527afdf83d430d5be5
SMAIL KIRA <108758834+kirazizi@users.noreply.github.com>
2023-11-11 21:48:33 +0100

    Update installer_find.sh

--- installer_find.sh
+++ installer_find.sh
@@ -11,7 +11,7 @@ set_new_alias() {
     shell_f="${HOME}/.${shell_f}rc"
 
     # Define the alias
-    alias_cmd="alias peerfinder='bash ${HOME}/1337-find-peers/find_peers.sh'"
+    alias_cmd="alias peerfinder='bash ${HOME}/1337_find_peers/find_peers.sh'"
 
     # Add the alias to the shell configuration file if it doesn't exist
     if ! grep -q "$alias_cmd" "$shell_f"; then
@@ -29,7 +29,7 @@ move_project_dir_to_home () {
     # Move the current project directory to the home directory
     current_dir="$(pwd)"
     cd ..
-    mv "${current_dir}" "${HOME}"
+    mv "$current_dir" "$HOME/1337_find_peers"
 }
 
 
