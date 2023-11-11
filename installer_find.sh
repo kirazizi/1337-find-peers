deaab9ccf871d950a156f801a34f4a2b0cd1af58
SMAIL KIRA <108758834+kirazizi@users.noreply.github.com>
2023-11-11 21:10:09 +0100

    add alias for script

--- installer_find.sh
+++ installer_find.sh
@@ -11,19 +11,20 @@ set_new_alias() {
     shell_f="${HOME}/.${shell_f}rc"
 
     # Define the alias
-    alias_cmd="alias findpeers='bash <(curl -s https://raw.githubusercontent.com/kirazizi/1337-find-peers/master/find_peers.sh)'"
+    alias_cmd="alias peerfinder='bash <(curl -s https://raw.githubusercontent.com/kirazizi/1337-find-peers/master/find_peers.sh)'"
 
     # Add the alias to the shell configuration file if it doesn't exist
     if ! grep -q "$alias_cmd" "$shell_f"; then
         echo -e "\n\n$alias_cmd" >> "$shell_f"
         echo -e "
 +--------------------------------------------------------+
-| Run this command \"${RED}source $shell_f${NO_COLOR}\" |
-| to be able to run the script directly by typing          |
-| \"${CYAN}findpeers${NO_COLOR}\" in your terminal.               |
+| Run this command \"${RED}source $shell_f${NO_COLOR}\"  |
+| to be able to run the script directly by typing        |
+| \"${CYAN}peerfinder${NO_COLOR}\" in your terminal.      |
 +--------------------------------------------------------+"
     fi
 }
 
 # Call the function to set the new alias
 set_new_alias
+
