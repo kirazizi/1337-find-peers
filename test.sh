6332ae3a1a8c875346555371a20cbf37f1b8104e
Smail Bzizal <sbzizal@e3r8p2.1337.ma>
2023-11-11 17:08:56 +0100

    1337 find peers

---
+++ test.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+# Create a virtual environment
+python3 -m venv venv
+
+# Activate the virtual environment
+source venv/bin/activate
+
+# Install the requests library using pip
+pip install requests
+
+# Run the script
+python src/find_peers.py
+
+# Deactivate the virtual environment
+deactivate
+
+# Remove the virtual environment
+rm -rf venv
+
+# Display a message indicating the script has completed
+echo "Script completed."
